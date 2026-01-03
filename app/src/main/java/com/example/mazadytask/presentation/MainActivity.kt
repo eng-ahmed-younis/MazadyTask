2382789ec4e777a90e2d2d7486711cb891d7e2e2
Ahmed Aly <ahmed.aly@gen-c.com>
2026-01-03 23:07:08 +0200

    Implement network connectivity observer in launch details; handle offline state and error dialog

--- app/src/main/java/com/example/mazadytask/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mazadytask/presentation/MainActivity.kt
@@ -7,14 +7,6 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mazadytask.presentation.navigation.AppNavHost
 import com.example.mazadytask.presentation.ui.theme.MazadyAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -39,11 +31,7 @@ class MainActivity : ComponentActivity() {
             MazadyAppTheme (
                 darkTheme = isDarkTheme
             ){
-                AppNavHost(
-                  /*  modifier = Modifier.windowInsetsPadding(0
-                       // WindowInsets.systemBars
-                    )*/
-                )
+                AppNavHost()
             }
         }
     }
